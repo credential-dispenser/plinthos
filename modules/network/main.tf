ce5b743f6954062c9b5141841aa7501b1010cc65
Kostas Nikolaou <kostas.nikolaou@contino.io>
2019-10-17 12:05:13 +0100

    Networking complete

---
+++ modules/network/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+resource "google_compute_network" "vpc" {
+    name = var.vpc_name
+    description = "A VPC for hosting the GKE app"
+    auto_create_subnetworks = false
+    routing_mode = "REGIONAL"
+    delete_default_routes_on_create = true
+}
+
+resource "google_compute_subnetwork" "public_subnet" {
+    name = var.public_subnet_name
+    network = google_compute_network.vpc.self_link
+    ip_cidr_range = var.public_subnet_cidr_ip
+}
+
+resource "google_compute_subnetwork" "private_subnet" {
+    name = var.private_subnet_name
+    network = google_compute_network.vpc.self_link
+    ip_cidr_range = var.private_subnet_cidr_ip
+}

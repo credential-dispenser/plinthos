d88bf27f03fa2b8f2cff3c81d945b978a036b46f
Kostas Nikolaou <kostas.nikolaou@contino.io>
2019-11-04 09:24:49 +0000

    Added GKE module back

--- prod/main.tf
+++ prod/main.tf
@@ -12,3 +12,22 @@ module "subnets" {
   subnet_cidr_ips = var.subnet_cidr_ips
   region = var.region
 }
+
+module "gke" {
+  source = "../modules/gke"
+
+  gke_cluster_name = "gke_cluster"
+  gcp_network_name = var.gcp_network_name
+  public_subnet_name = var.public_subnet_name
+  cluster_secondary_range_name = module.network.cluster_secondary_range_name
+  services_secondary_range_name = module.network.services_secondary_range_name
+  region = var.region
+  project = var.project
+  zone = var.zone
+  master_username = var.master_username
+  master_password = var.master_password
+  gke_node_pool_name_1 = var.gke_node_pool_name_1
+  node_count = var.node_count
+  machine_type = var.machine_type
+  office_ip = var.office_ip
+}

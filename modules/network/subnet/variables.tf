5f73ffed15a051b690c5f17964aee23439dae26d
Kostas Nikolaou <kostas.nikolaou@contino.io>
2019-11-04 09:15:31 +0000

    Broken down network to modular

--- modules/network/subnet/variables.tf
+++ modules/network/subnet/variables.tf
@@ -15,3 +15,17 @@ variable "subnet_cidr_ips" {
 variable "region" {
     description = "Region to deploy the subnets"
 }
+
+# variable "ip_cidr_ranges" {
+#     type = map(list)
+#     default = {
+#         gg = ["172.20.0.0/16", "172.20.0.0/16"],
+#         wp = []
+#     }
+#     # default = [["172.20.0.0/16", "172.20.0.0/16"],[]]
+# }
+
+# variable "range_names" {
+#     type = list(string)
+#     default = [["container-range-1", "service-range-1"],[]]
+# }
